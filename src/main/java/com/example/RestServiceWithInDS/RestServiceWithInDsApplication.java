d377322f303d1aa437acabd290c8a9bffb7aeb0f
Kavita14995 <kavitakumawat14995@gmail.com>
2021-08-08 03:09:29 +0530

    initial commit

---
+++ src/main/java/com/example/RestServiceWithInDS/RestServiceWithInDsApplication.java
@@ -0,0 +1,13 @@
+package com.example.RestServiceWithInDS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestServiceWithInDsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestServiceWithInDsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/RestServiceWithInDS/RestServiceWithInDsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RestServiceWithInDS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestServiceWithInDsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
